aef747d53f4010e6808e9eb3c5dfdd67a4a1f2ad
Andriy Skoropad <andry.skoropad@gmail.com>
2016-06-28 17:06:34 +0300

    make this witout animation

--- grafik_1/script_graf.js
+++ grafik_1/script_graf.js
@@ -11,15 +11,25 @@ window.onload = function(){
     settings.colorMin = "#000E8F";
     settings.myData = ["value", "done"];
     settings.deafaultMaximumForColor = 3000;
+    settings.margin = {
+        top : 20,
+        right : 20,
+        bottom : 20,
+        left : 20
+    };
+    settings.transitioningTime = 700;
 
+    var transitioning = false;
     var maxCount = NaN;
     var minCount = 0;
+    var main = null;
 
     //------- settings block end ------
 
     //------- object prepear ----------
     function accumulate(object, value) {
-        if ( object.children){
+        if ( object.children ){
+            object._children = object.children
             return object[value] = object.children.reduce(function(p, v) { return p + accumulate(v, value); }, 0);
         } else {
             return object[value];
@@ -34,6 +44,29 @@ window.onload = function(){
         console.log( "compile done >>")
     }
 
+    // coordinates. This lets us use a viewport to zoom.
+    function layout(object) {
+        if (object._children) {
+              treemap.nodes({_children: object._children});
+              
+              object._children.forEach(function(c) {
+                    c.x = object.x + c.x * object.dx;
+                    c.y = object.y + c.y * object.dy;
+                    c.dx *= object.dx;
+                    c.dy *= object.dy;
+                    c.parent = object;
+                    layout(c);
+              });
+        }
+    }
+
+    function initialize(object) {
+        console.log('>>>', object);
+        object.x = object.y = 0;
+        object.dx = settings._mainBlockWidth;
+        object.dy = settings._mainBlockHeight;
+        object.depth = 0;
+    }
 
     //------- svg start ---------------
     var svg = d3.select(settings.mainBlock).append("svg")
@@ -55,40 +88,107 @@ window.onload = function(){
         .domain([0, settings._mainBlockHeight])
         .range([0, settings._mainBlockHeight]);
 
-    function loadData( data ){
-
-        accumulateAll(settings.myData, data);//prepear data
-
-        console.log('>>>load done and data is >>>', data);
-
-        var treemap = d3.layout.treemap()
-            .children(function(d, depth) { return depth ? null : d.children; })
+    var treemap = d3.layout.treemap()
+            .children(function(d, depth) { return depth ? null : d._children; })
             .size([settings._mainBlockWidth, settings._mainBlockHeight])
-            .nodes( data );
-        console.log( treemap );
+            .sort(function(a, b) { return a.value - b.value; })
+            .round(false);
 
-        var cells = svg.selectAll( "." + settings.classesForCell)
-            .data( treemap )
-            .enter()
-            .append("g")
-            .attr("class", settings.classesForCell);
-
-        cells.append("rect")
-            .attr("x",function(d){return x(d.x); })
-            .attr("y",function(d){return y(d.y); })
-            .attr("width",function(d){return x(d.dx); })
-            .attr("height",function(d){return y(d.dy); })
-            .attr("fill", function(d){return d.done ? color(d.done) : null;})
-            .attr("stroke", "#fff")
-
-        cells.append("text")
-            .attr("x",function(d){return x(d.x); })
-            .attr("y",function(d){return y(d.y ); })
-            .attr("dy", 20)
-            .text(function(d){return d.name;})
-            .attr("fill", "#000")
+    function loadData( data ){
+        initialize(data);//prepear data main block
+        accumulateAll(settings.myData, data);//prepear data
+        layout(data);//prepear data fo zoom
+        console.log('>>>load done and data is >>>', data);
+        showSvg(data)
+        
     };
 
     d3.json("../data.json", loadData);
 
+    function showSvg(data){
+        var mainTreemap = treemap.nodes( data );
+        console.log("done",  mainTreemap );
+
+        main = svg.insert("g", ".grandparent")
+            .datum(data)
+            .attr("class", "depth");
+
+        var cells = main.selectAll( "." + settings.classesForCell)
+            .data( mainTreemap )
+            .enter()
+            .append("g")
+            .attr("class", settings.classesForCell);
+            
+
+        cells.append("rect")
+            .call(rect);
+
+        cells.append("text")
+            .call(text);
+
+        cells.filter(function(d) { return d._children; })
+            .classed("children", true)
+            .on("click", transition);
+
+        return cells;
+    }
+
+    function transition (data){
+        console.log(data);
+
+        if (transitioning || !data) return;
+        showSvg(data)
+        transitioning = true;
+        var mainNew = showSvg(data);
+        var beforeClick = main.transition().duration(750);
+        var afterClick = mainNew.transition().duration(750);
+
+        // Update the domain only after entering new elements.
+        x.domain([data.x, data.x + data.dx]);
+        y.domain([data.y, data.y + data.dy]);
+
+        // Enable anti-aliasing during the transition.
+        svg.style("shape-rendering", null);
+
+        // Draw child nodes on top of parent nodes.
+        svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth; });
+
+        // Fade-in entering text.
+        mainNew.selectAll("text").style("fill-opacity", 0);
+
+        // Transition to the new view.
+        beforeClick.selectAll("text").call(text).style("fill-opacity", 0);
+        afterClick.selectAll("text").call(text).style("fill-opacity", 1);
+        beforeClick.selectAll("rect").call(rect);
+        afterClick.selectAll("rect").call(rect);
+
+        // Remove the old node when the transition is finished.
+        beforeClick.remove().each("end", function() {
+            svg.style("shape-rendering", "crispEdges");
+            transitioning = false;
+        });
+    }
+
+
+    function text(text) {
+        text.attr("x",function(d){return x(d.x); })
+            .attr("y",function(d){return y(d.y ); })
+            .attr("dy", 20)
+            .text(function(d){return d.name;})
+            .attr("fill", "#000");
+    }
+
+    function rect(rect) {
+        rect.attr("x",function(d){return x(d.x); })
+            .attr("y",function(d){return y(d.y); })
+            .attr("width",function(d){return x(d.dx); })
+            .attr("height",function(d){return y(d.dy); })
+            .attr("fill", function(d){return d.done ? color(d.done) : null;})
+            .attr("stroke", "#fff");
+    }
+
+
+
+
 };
+
