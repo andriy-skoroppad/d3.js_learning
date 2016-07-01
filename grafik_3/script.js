e4fa8a9e76ccb7b8db23079c79b1546db9ae0f03
deepinspire:Andriy Skoropad <andriy.skoropad@deepinspire.com>
2016-07-01 18:08:49 +0300

    v4

---
+++ grafik_3/script.js
@@ -0,0 +1,125 @@
+window.onload = function(){
+    d3.json("../data.json", loadData);
+
+    var element = document.querySelector(".chart");
+
+    var canvasForTreemap = d3.select(element).style("overflow", "hidden");
+
+    function loadData(data){
+        initialize(data);
+
+        accumulateAll(["value", "done"], data);
+
+        layout(data);
+        console.log(data);
+
+        var svgForTreemap =  canvasForTreemap.append("svg")
+            .attr("height", 150)
+            .attr("width", 150);
+
+        var main = svgForTreemap.append("g")
+            .datum(data)
+            .attr("class", "depth");
+
+        var children = main.selectAll("g")
+            .data(data._children)
+            .enter()
+            .append("g");
+
+        children.filter(function(d){ return d._children})
+            .classed("children", true)
+            .on("click", transition);
+
+        children.append("rect")
+            .attr("class", "parent")
+            .call(rect)
+            .append("title")
+            .text(function(d){ return d3.format(",d")(d.value); })
+    }
+
+
+    function transition(data){
+        console.log(data);
+    }
+
+    //------- object prepear ----------
+    function initialize(object) {
+        console.log('>>>', object);
+        object.x = object.y = 0;
+        object.dx = 150;
+        object.dy = 150;
+        object.depth = 0;
+    }
+
+    function accumulate(object, value) {
+        if ( object.children ){
+            object._children = object.children
+            return object[value] = object.children.reduce(function(p, v) { return p + accumulate(v, value); }, 0);
+        } else {
+            return object[value];
+        };
+
+    }
+
+    function accumulateAll(array, object){
+        for (var i = 0; i < array.length; i++) {
+            accumulate(object, array[i])
+        }
+        console.log( "compile done >>")
+    }
+
+    function layout(object) {
+        if (object._children) {
+            treemap.nodes({_children: object._children});
+
+            object._children.forEach(function(c) {
+                c.x = object.x + c.x * object.dx;
+                c.y = object.y + c.y * object.dy;
+                c.dx *= object.dx;
+                c.dy *= object.dy;
+                c.parent = object;
+                layout(c);
+            });
+        }
+    }
+
+
+
+
+    var root = d3.hierarchy(data);
+
+    var  nodes = d3.treemap()
+
+        .round(false);
+
+    var treemap = nodes(root
+        .children(function(d, depth) { return depth ? null : d._children; })
+        .sort(function(a, b) { return a.value - b.value; })
+        //.ratio(150 / 150 * 0.5 * (1 + Math.sqrt(5)))
+    )
+        .descendants();
+
+
+
+    var color =  d3.scaleLinear()//градієнт кольорів відносно величини
+        .domain([0, 1000])
+        .range(["#7fffd4" , "#2a3c01" ]);
+
+    var x = d3.scaleLinear()
+        .domain([0, 150])
+        .range([0, 150]);
+
+    var y = d3.scaleLinear()
+        .domain([0, 150])
+        .range([0, 150]);
+
+    function rect(rect) {
+        rect.attr("x",function(d){return x(d.x); })
+            .attr("y",function(d){return y(d.y); })
+            .attr("width",function(d){return x(d.x + d.dx) - x(d.x); })
+            .attr("height",function(d){return y(d.y + d.dy) - y(d.y); })
+            .attr("fill", function(d){return d.done ? color(d.done) : null;})
+            .attr("stroke", "#fff");
+    }
+
+};
